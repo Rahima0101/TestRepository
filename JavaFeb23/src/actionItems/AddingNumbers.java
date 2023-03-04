036ce2581dcc0532bbfedf5ad6a3c9a972ec4ec3
Rahima0101 <rahima.akter123@gmail.com>
2023-03-04 12:52:08 -0500

    Add changes to ADDONG NUMBERS AMD ARRAY DEMO

---
+++ JavaFeb23/src/actionItems/AddingNumbers.java
@@ -0,0 +1,16 @@
+package actionItems;
+
+public class AddingNumbers {
+
+	public static void main(String[] args) {
+		// write a program that sums the numbers from 0 to 15 using for, while and do while
+		
+		int sum = 0;
+		for(int i = 0; i<=15; i++)
+		{
+			sum = sum + i;
+		}
+         System.out.println(" The result is: " + sum);
+	}
+
+}
